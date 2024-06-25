4cef7eecb64ca26ce88dba38518eed37b8f05388
GonzzaloBarbosa <101589640+GonzzaloBarbosa@users.noreply.github.com>
2024-06-25 19:37:07 +0100

    Retirar OpportuniyuList

--- unity-app/src/App.js
+++ unity-app/src/App.js
@@ -1,14 +1,12 @@
 import React from 'react';
 import './styles/App.css';
 import NavBar from './componentes/NavBar';
-import OpportunityList from './componentes/OpportunityList';
 
 function App() {
   return (
     <div className="App">
       <NavBar />
       <div className="content">
-        <OpportunityList />
       </div>
     </div>
   );
--- unity-app/src/componentes/NavBar.js
+++ unity-app/src/componentes/NavBar.js
@@ -5,31 +5,10 @@ import { MenuItens } from './MenuItens';
 
 //componente da classe
 class Navbar extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            searchTerm: '' // Estado para armazenar o termo de pesquisa
-        };
-    }
-
-    handleSearchChange = (event) => {
-        this.setState({ searchTerm: event.target.value });
-    }
     render(){
         return(
             <nav className="NavbarItems">
                 <h1 className="navbar-logo">UnityHub</h1>
-                {/* Barra de navegação e pesquisa */}
-                <div className="nav-search">
-                    <input 
-                        type="text" 
-                        placeholder="Pesquisar..." 
-                        value={this.state.searchTerm} 
-                        onChange={this.handleSearchChange} 
-                        className="search-input"
-                    />
-                    <i className="fa-solid fa-magnifying-glass search-icon"></i>
-                </div>
                 <ul className="nav-menu">
                     {MenuItens.map((item, index) =>{
                         return(
